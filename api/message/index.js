20b6b861760b8b95c9b39e3f2f87f7cff56e96a1
Florian Georg <florian.alexander.georg@gmail.com>
2023-07-04 16:29:04 +0000

    made a change

--- api/message/index.js
+++ api/message/index.js
@@ -1,6 +1,9 @@
 module.exports = async function (context, req) {
     const someCredential = process.env.SOME_CREDENTIAL;
+    // add some logic here to get the credential from a key vault or something
+
+
     context.res.json({
-        text: `Hello from the API. Our credential  is: ${someCredential}`
+        text: `Hello from ETH. Our credential  is: ${someCredential}`
     });
 };
