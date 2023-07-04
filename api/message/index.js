a60e849c65f2918d4537927c1ed18befa8873ef5
Florian Georg <florian.alexander.georg@gmail.com>
2023-07-04 12:55:27 +0000

    changes to devcontainer

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,6 @@
 module.exports = async function (context, req) {
+    const databaseName = process.env.DATABASE_NAME;
     context.res.json({
-        text: "Hello from the API"
+        text: `Hello from the API. Our Datebase name is ${databaseName}`
     });
 };
--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
+const connectionString = process.env.DATABASE_CONNECTION_STRING;
+
+
 function App() {
   const [data, setData] = useState('');
 
